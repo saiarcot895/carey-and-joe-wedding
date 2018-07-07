fc24b7b526b97813ea0be331aca1d499ec440cce
Saikrishna Arcot <saiarcot895@gmail.com>
2018-07-06 22:42:40 -0700

    Add frontend code to get RSVP information about the guests.

--- main.js
+++ main.js
@@ -4,4 +4,27 @@ $(function() {
             scrollTop: $($(this).attr("href")).offset().top - 56
         });
     });
+
+    $("#rsvpContainer form").submit(function(e) {
+        // Prevent default functionality
+        e.preventDefault();
+
+        // Get the action URL of the form
+        var actionurl = $(this).find("button").prop("formAction");
+
+        // Do our own request and handle the results
+        $.ajax({
+            url: actionurl,
+            type: "POST",
+            dataType: "json",
+            data: $(this).find("input[name=firstName], input[name=lastName]").serialize(),
+            jsonp: false,
+            success: function(data) {
+                $("#numGuestsInput").attr("value", data["numGuests"]);
+                $("#attendingDay1Check").prop("checked", data["presentForDay1"]);
+                $("#attendingDay2Check").prop("checked", data["presentForDay2"]);
+                $("#rsvpContainer form fieldset:disabled").prop("disabled", false).slideDown();
+            }
+        });
+    });
 });
